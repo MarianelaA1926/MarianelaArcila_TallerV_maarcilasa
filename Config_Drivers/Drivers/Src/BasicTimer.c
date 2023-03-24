a5f893cb0692f7ceb087f8ab2607623fa03f05e4
MarianelaA1926 <maarcilasa@unal.edu.co>
2023-03-23 22:51:35 -0500

    BasicTimer
    We activate the clock signal

--- Config_Drivers/Drivers/Src/BasicTimer.c
+++ Config_Drivers/Drivers/Src/BasicTimer.c
@@ -28,8 +28,40 @@ void BasicTimer_Config(BasicTimer_Handler_t *ptrBTimerHandler){
 	//We keep a reference to the peripheral that we are using
 	ptrTimerUsed = ptrBTimerHandler -> ptrTIMx;
 
-
+	// First we deactivate global interruptions while we configure the timer system
 	__disable_irq();
+
+	// The second step is to activate the signal clock of the required pheriferal
+	if(ptrBTimerHandler -> ptrTIMx == TIM2){
+		// We write 1 (SET)  in the position corresponds to TIM2
+		RCC -> APB1ENR |= (SET << RCC_APB1ENR_TIM2EN);
+	}
+	if(ptrBTimerHandler -> ptrTIMx == TIM3){
+			// We write 1 (SET)  in the position corresponds to TIM3
+			RCC -> APB1ENR |= (SET << RCC_APB1ENR_TIM3EN);
+	}
+	if(ptrBTimerHandler -> ptrTIMx == TIM4){
+			// We write 1 (SET)  in the position corresponds to TIM4
+			RCC -> APB1ENR |= (SET << RCC_APB1ENR_TIM4EN);
+	}
+	if(ptrBTimerHandler -> ptrTIMx == TIM5){
+			// We write 1 (SET)  in the position corresponds to TIM5
+			RCC -> APB1ENR |= (SET << RCC_APB1ENR_TIM5EN);
+	}
+	if(ptrBTimerHandler -> ptrTIMx == TIM9){
+			// We write 1 (SET)  in the position corresponds to TIM10
+			RCC -> APB2ENR |= (SET << RCC_APB2ENR_TIM9EN);
+	}
+	if(ptrBTimerHandler -> ptrTIMx == TIM10){
+			// We write 1 (SET)  in the position corresponds to TIM2
+			RCC -> APB2ENR |= (SET << RCC_APB2ENR_TIM10EN);
+	}
+	if(ptrBTimerHandler -> ptrTIMx == TIM11){
+			// We write 1 (SET)  in the position corresponds to TIM2
+			RCC -> APB1ENR |= (SET << RCC_APB2ENR_TIM9EN);
+	}
+
+
 
 
 
