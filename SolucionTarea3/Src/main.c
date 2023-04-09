5a1218065b54747d5415e7ecbe548098c1358466
MarianelaA1926 <maarcilasa@unal.edu.co>
2023-04-09 11:35:41 -0500

    SolucionTarea3
    Encoder

--- SolucionTarea3/Src/main.c
+++ SolucionTarea3/Src/main.c
@@ -31,15 +31,29 @@
 // handlers  GPIOA pin A5 y  de TIM2
 GPIO_Handler_t handlerStateLed = {0};
 BasicTimer_Handler_t handlerTimerStateLed = {0};
-//---------------------------------------------------------ENCODER-------------------------------------------------------
+//---------------------------------------------------------Buton-------------------------------------------------------
 
 
-GPIO_Handler_t handlerEncoderLed = {0};
+GPIO_Handler_t handlerExternalLed = {0};
 
-GPIO_Handler_t handlerEncoderButton = {0};
+GPIO_Handler_t handlerExternalButton = {0};
 EXTI_Config_t handlerExtiButton;
 
 uint8_t buttonPressed;
+//------------------------------------------------------Encoder-----------------------------------------------------------------
+
+GPIO_Handler_t handlerEncoderClock = {0};
+EXTI_Config_t handlerExtiEncoderCK = {0};
+uint8_t lastClkState;
+
+
+GPIO_Handler_t handlerEncoderDT = {0};
+uint8_t lastDTState;
+
+
+GPIO_Handler_t handlerEncoderSW = {0};
+EXTI_Config_t handlerExtiEncoderSW = {0};
+
 
 /* Principal Functions */
 void initSystem(void);
@@ -53,6 +67,10 @@ int main(void)
 
     /* Principal cicle  */
 	while(1){
+
+
+
+
 
 	}
 	return 0;
@@ -87,34 +105,77 @@ void initSystem(void){
 	BasicTimer_Config(&handlerTimerStateLed);
 	// We activate the TIM2
 	starTimer(&handlerTimerStateLed);
-//------------------------------------------------Encoder-----------------------------------------------------
+//------------------------------------------------External Butom-----------------------------------------------------
 
 	// Pórt GPIOC pin 8
-	handlerEncoderLed.pGPIOx = 											GPIOC;
-	handlerEncoderLed.GPIO_PinConfig.GPIO_PinNumber = 					PIN_8;
-	handlerEncoderLed.GPIO_PinConfig.GPIO_PinMode = 					GPIO_MODE_OUT;
-	handlerEncoderLed.GPIO_PinConfig.GPIO_PinOPType = 					GPIO_OTYPE_PUSHPULL;
-	handlerEncoderLed.GPIO_PinConfig.GPIO_PinPuPdControl = 				GPIO_PUPDR_PULLDOWN;
-	handlerEncoderLed.GPIO_PinConfig.GPIO_PinSpeed = 					GPIO_OSPEED_FAST;
+	handlerExternalLed.pGPIOx = 											GPIOC;
+	handlerExternalLed.GPIO_PinConfig.GPIO_PinNumber = 					PIN_8;
+	handlerExternalLed.GPIO_PinConfig.GPIO_PinMode = 					GPIO_MODE_OUT;
+	handlerExternalLed.GPIO_PinConfig.GPIO_PinOPType = 					GPIO_OTYPE_PUSHPULL;
+	handlerExternalLed.GPIO_PinConfig.GPIO_PinPuPdControl = 				GPIO_PUPDR_PULLDOWN;
+	handlerExternalLed.GPIO_PinConfig.GPIO_PinSpeed = 					GPIO_OSPEED_FAST;
 
 
 	//We load the configuration of the specific pin
-	//GPIO_WritePin(&handlerEncoderLed, SET);
-	GPIO_Config(&handlerEncoderLed);
+	//GPIO_WritePin(&handlerExternalLed, SET);
+	GPIO_Config(&handlerExternalLed);
 
 
 	/*Button configuration as external interruption*/
 	//GPIO configuration
-	handlerEncoderButton.pGPIOx	= 										GPIOB;
-	handlerEncoderButton.GPIO_PinConfig.GPIO_PinNumber = 				PIN_8;
-	handlerEncoderButton.GPIO_PinConfig.GPIO_PinMode = 					GPIO_MODE_IN;
-	handlerEncoderButton.GPIO_PinConfig.GPIO_PinPuPdControl = 			GPIO_PUPDR_PULLUP;
+	handlerExternalButton.pGPIOx	= 										GPIOB;
+	handlerExternalButton.GPIO_PinConfig.GPIO_PinNumber = 				PIN_8;
+	handlerExternalButton.GPIO_PinConfig.GPIO_PinMode = 					GPIO_MODE_IN;
+	handlerExternalButton.GPIO_PinConfig.GPIO_PinPuPdControl = 			GPIO_PUPDR_PULLUP;
 
 
 	//ExtiConfiguration
-	handlerExtiButton.pGPIOHandler= 									&handlerEncoderButton;
+	handlerExtiButton.pGPIOHandler= 									&handlerExternalButton;
 	handlerExtiButton.edgeType = 										EXTERNAL_INTERRUPT_RISING_EDGE;
 	extInt_Config(&handlerExtiButton);
+
+
+
+//------------------------------------------Encoder----------------------------------------------------------------
+//---CLOCK-----------------------------------------------------
+	/*ENCODER CLOCK configuration as external interruption*/
+	//GPIO configuration
+	handlerEncoderClock.pGPIOx	= 										GPIOA;
+	handlerEncoderClock.GPIO_PinConfig.GPIO_PinNumber = 				PIN_0;
+	handlerEncoderClock.GPIO_PinConfig.GPIO_PinMode = 					GPIO_MODE_IN;
+	handlerEncoderClock.GPIO_PinConfig.GPIO_PinPuPdControl = 			GPIO_PUPDR_PULLUP;
+
+
+	//ExtiConfiguration
+	handlerExtiEncoderCK.pGPIOHandler= 									&handlerEncoderClock;
+	handlerExtiEncoderCK.edgeType = 										EXTERNAL_INTERRUPT_FALLING_EDGE;
+	extInt_Config(&handlerExtiEncoderCK);
+
+
+//-----DT--------------------------------------------------------
+	/*ENCODER DT configuration as external interruption*/
+	//GPIO configuration
+	handlerEncoderDT.pGPIOx	= 										GPIOA;
+	handlerEncoderDT.GPIO_PinConfig.GPIO_PinNumber = 				PIN_1;
+	handlerEncoderDT.GPIO_PinConfig.GPIO_PinMode = 					GPIO_MODE_IN;
+	handlerEncoderDT.GPIO_PinConfig.GPIO_PinPuPdControl = 			GPIO_PUPDR_PULLUP;
+
+
+
+//-----SW------------------------------------------------------------
+	/*ENCODER SW configuration as external interruption*/
+	//GPIO configuration
+	handlerEncoderSW.pGPIOx	= 										GPIOA;
+	handlerEncoderSW.GPIO_PinConfig.GPIO_PinNumber = 				PIN_2;
+	handlerEncoderSW.GPIO_PinConfig.GPIO_PinMode = 					GPIO_MODE_IN;
+	handlerEncoderSW.GPIO_PinConfig.GPIO_PinPuPdControl = 			GPIO_PUPDR_PULLUP;
+
+
+	//ExtiConfiguration
+	handlerExtiEncoderSW.pGPIOHandler= 									&handlerEncoderSW;
+	handlerExtiEncoderSW.edgeType = 										EXTERNAL_INTERRUPT_FALLING_EDGE;
+	extInt_Config(&handlerExtiEncoderSW);
+
 
 }
 
@@ -125,13 +186,47 @@ void initSystem(void){
 
 
 
-
+/*
 //Callback function with Exti
 void callback_extInt6(void){
 
 
 }
+*/
+//Callback function with Exti
+void callback_extInt0(void){
 
+//Se lee el estado del clok para determina si su funcionamiento es correcto
+	uint8_t clkState;
+	clkState = GPIO_ReadPin(&handlerEncoderClock);
+
+	//Se detecta si hay un flanco de subida o de bajada
+	if ( clkState != lastClkState){
+		lastClkState = clkState;
+
+		//Se lee el estado actual del pin DT con el fin de determinar como gira el encoder
+		lastDTState = GPIO_ReadPin(&handlerEncoderDT);
+
+		if(lastDTState == clkState){
+			//Giro en sentido horario se enciende el Led externo
+			GPIO_WritePin(&handlerExternalLed, SET);
+		}
+		else{
+			//Giro en sentido antiohorario se apaga el led
+			GPIO_WritePin(&handlerExternalLed, RESET);
+
+		}
+
+
+
+	}
+}
+
+//Callback function with Exti
+void callback_extInt2(void){
+
+
+}
 
 
 //Callback function with state led
@@ -145,12 +240,17 @@ void BasicTimer2_Callback(void){
 
 
 	// Lectura del botón
-	buttonPressed = GPIO_ReadPin(&handlerEncoderButton);
+	buttonPressed = GPIO_ReadPin(&handlerExternalButton);
     if (buttonPressed == RESET) { // Si el botón está presionado
-    	GPIO_WritePin(&handlerEncoderLed, RESET); // Apagamos el LED asociado
+    	GPIO_WritePin(&handlerExternalLed, RESET); // Apagamos el LED asociado
     } else { // Si el botón no está presionado
-    	GPIO_WritePin(&handlerEncoderLed, SET); // Encendemos el LED asociado
+    	GPIO_WritePin(&handlerExternalLed, SET); // Encendemos el LED asociado
     }
+
+
+
+
+
 
 
 }
