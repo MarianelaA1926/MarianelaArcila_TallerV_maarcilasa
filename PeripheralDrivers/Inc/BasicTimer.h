f1747514cf698c93eabf8b1d71005e67b34737d5
MarianelaA1926 <maarcilasa@unal.edu.co>
2023-05-17 17:14:56 -0500

    PeripheralDrivers
    We create the library with our peripherals drivers.

---
+++ PeripheralDrivers/Inc/BasicTimer.h
@@ -0,0 +1,74 @@
+/*
+ * BasicTimer.h
+ *
+ *  Created on: 22/03/2023
+ *      Author: Marianela Arcila Sanchez 			maarcilasa
+ * In this program, we define the functions needed to configure
+ * the general-purpose timer peripheral "TIMx.
+ *
+ */
+
+#ifndef INC_BASICTIMER_H_
+#define INC_BASICTIMER_H_
+
+#include "stm32f4xx.h"
+
+#define BTIMER_MODE_UP 				0
+#define BTIMER_MODE_DOWN			1
+
+
+#define BTIMER_SPEED_10us			160
+#define BTIMER_SPEED_100us			1600
+#define BTIMER_SPEED_1ms			16000
+
+
+#define BTIMER_DISABLE  0
+#define BTIMER_ENABLE   1
+
+/*This structure contains the definition of the variables that
+ * the user has chosen. */
+
+typedef struct{
+
+	uint8_t 			TIMx_mode;						//Up or Down
+	uint32_t 			TIMx_speed;						// Speed at which the timer increases
+	uint32_t 			TIMx_period;					// Value for the period of the timer in miliseconds (ms
+	uint8_t				TIMx_interrupEnable;			// Timer interrupt mode (On, Off)
+
+}BasicTimer_Config_t;
+
+/*Timer handler*/
+
+typedef struct{
+
+	TIM_TypeDef 					*ptrTIMx;
+	BasicTimer_Config_t 			TIMx_Config;
+
+}BasicTimer_Handler_t;
+
+
+void BasicTimer_Config(BasicTimer_Handler_t *ptrBTimerHandler);
+void BasicTimer2_Callback(void); /*This function overwrites the 'main' so the system works.*/
+void BasicTimer3_Callback(void); /*This function overwrites the 'main' so the system works.*/
+void BasicTimer4_Callback(void); /*This function overwrites the 'main' so the system works.*/
+void BasicTimer5_Callback(void); /*This function overwrites the 'main' so the system works.*/
+
+void starTimer (BasicTimer_Handler_t *ptrBTimerHandler);
+
+void stopTimer(BasicTimer_Handler_t *ptrBTimerHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* INC_BASICTIMER_H_ */
---
+++ PeripheralDrivers/Src/GPIOxDriver.c
@@ -0,0 +1,246 @@
+/*
+ * GPIOxDriver.c
+ *
+ *  Created on: 13/03/2023
+ *      Author: Marianela Arcila Sanchez
+ *  In this program, we write about the control of the GPIO peripheral.
+ *  This control aims to be as generic as possible.
+ */
+
+
+/*First, we need to activate the clock signal. This is very important because
+ * the clock signal is used to synchronize the different components of the
+ *  microcontroller. (RCC)
+ */
+#include "GPIOxDriver.h"
+
+void GPIO_Config (GPIO_Handler_t *pGPIOHandler ){
+
+	//create variable
+	uint32_t auxConfig = 0;
+	uint32_t auxPosition = 0;
+
+	// Activate Peripheric
+	// For GPIOA
+	if(pGPIOHandler -> pGPIOx == GPIOA){
+	// We write 1 (SET)  in the position corresponds to GPIOA
+		RCC -> AHB1ENR |= (SET << RCC_AHB1ENR_GPIOAEN_Pos);
+	}
+	// For GPIOB
+	else if(pGPIOHandler -> pGPIOx == GPIOB){
+	// We write 1 (SET)  in the position corresponds to GPIOB
+		RCC -> AHB1ENR |= (SET << RCC_AHB1ENR_GPIOBEN_Pos);
+	}
+	// For GPIOC
+	else if(pGPIOHandler -> pGPIOx == GPIOC	){
+	// We write 1 (SET)  in the position corresponds to GPIOC
+		RCC -> AHB1ENR |= (SET << RCC_AHB1ENR_GPIOCEN_Pos);
+	}
+	// For GPIOD
+	else if(pGPIOHandler -> pGPIOx == GPIOD ){
+	// We write 1 (SET)  in the position corresponds to GPIOD
+		RCC -> AHB1ENR |= (SET << RCC_AHB1ENR_GPIODEN_Pos);
+	}
+	// For GPIOE
+	else if(pGPIOHandler -> pGPIOx == GPIOE	){
+	// We write 1 (SET)  in the position corresponds to GPIOE
+		RCC -> AHB1ENR |= (SET << RCC_AHB1ENR_GPIOEEN_Pos);
+	}
+	// For GPIOH
+	else if(pGPIOHandler -> pGPIOx == GPIOH	){
+	// We write 1 (SET)  in the position corresponds to GPIOH
+		RCC -> AHB1ENR |= (SET << RCC_AHB1ENR_GPIOHEN_Pos);
+	}
+
+	/* Then we star to configure the  GPIO registers*/
+
+
+
+
+	/* The second step is to configure the GPIOx_MODER register.
+	 * This register indicates the mode of operation of the pin
+	 * that you want to use. (input, output, alternative function, analog mode)
+	 */
+
+	/*"We read the 'config' given by the user,
+	 * and then we move 'Pin Number' times to  the right using a 'shift left' operation."
+	 * This is in auxConfig
+	 */
+
+	auxConfig = (pGPIOHandler -> GPIO_PinConfig.GPIO_PinMode << 2 * pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber);
+
+	/* We clear the bits in the register before loading the new value.
+	 * for this step, we use a mask and operation Bitwise AND (0b000)*
+	 */
+	pGPIOHandler -> pGPIOx -> MODER &= ~(0b11 << 2 * pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber);
+
+	/*We load auxConfig to the MODER register*/
+
+	pGPIOHandler -> pGPIOx ->MODER |= auxConfig;
+
+
+
+
+
+
+	/* The third step is to configure the GPIOx_TYPER register. In this register,
+	 * we define whether the pin will be configured as an output push-pull (0 - 1) or an output open-drain.
+	 * We read and move the value "PinNumber" times
+	 */
+	auxConfig = (pGPIOHandler -> GPIO_PinConfig.GPIO_PinOPType << pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber);
+
+	/*We clear before to loading the new value*/
+	pGPIOHandler -> pGPIOx ->OTYPER &= ~(SET << pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber);
+
+	/*We load the new value for the register */
+	pGPIOHandler -> pGPIOx ->OTYPER |= auxConfig;
+
+
+
+
+
+	/* The fourth step is to configure the speed of the output,
+	 *  register  GPIOx_OSPEEDR*/
+
+	auxConfig = (pGPIOHandler -> GPIO_PinConfig.GPIO_PinSpeed << 2*pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber);
+
+	/*We clear before to loading the new value*/
+	pGPIOHandler ->pGPIOx ->OSPEEDR &=  ~(0b11 << 2* pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber);
+
+	/*We load the new value for the register*/
+	pGPIOHandler -> pGPIOx ->OSPEEDR |= auxConfig;
+
+
+
+
+	/*The fifth step is to configure the GPIOx_PUPDR register,
+	 *in which we establish a defined state for the signal. (Pull-up, pull-down) */
+
+	auxConfig = (pGPIOHandler -> GPIO_PinConfig.GPIO_PinPuPdControl << 2*pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber);
+
+	/*We clear the register position before loading the new configuration*/
+	pGPIOHandler -> pGPIOx -> PUPDR &= ~ (0b11 << 2* pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber);
+
+	/*We Load the new value*/
+	pGPIOHandler ->pGPIOx ->PUPDR |= auxConfig;
+
+
+
+	/*The sixth step is to configurate the alternative funtions GPIOx_ALTFN
+	 * The alternative functions are additional functions such as serial communication (USART),
+	 * analog interfaces (ADC), among others, which are assigned to specific pins."
+	 */
+	if(pGPIOHandler -> GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN){
+
+		/* We select the register AFRL for the pins from 0 to 7 or
+		 * the register AFRH for the pins from 8 to 15  */
+		if(pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber < 8){
+			//Register AFRL
+			auxPosition = 4 * pGPIOHandler ->GPIO_PinConfig.GPIO_PinNumber;
+
+			//We clear the position to the register AFRL
+			pGPIOHandler -> pGPIOx -> AFR[0] &= ~(0b1111 << auxPosition);
+
+			// We write the value into the AFRL register
+			pGPIOHandler -> pGPIOx ->AFR[0] |= (pGPIOHandler -> GPIO_PinConfig.GPIO_PinAltFunMode << auxPosition);
+		}
+		else{
+
+			//Register AFRH
+			auxPosition = 4 * (pGPIOHandler -> GPIO_PinConfig.GPIO_PinNumber -8);
+			// We clear the position to the register AFRH
+			pGPIOHandler ->pGPIOx -> AFR[1] &= ~(0b1111 << auxPosition);
+
+			// We write the value into the AFRH register
+			pGPIOHandler ->pGPIOx ->AFR[1] |= (pGPIOHandler ->GPIO_PinConfig.GPIO_PinAltFunMode << auxPosition);
+		}
+
+	}
+
+} // The end GPIO_config
+
+
+
+/*
+ * Then we create a new function. In this function,
+ * we change the state of the selected pin according
+ * to the user's input in the handler.
+ */
+
+void GPIO_WritePin(GPIO_Handler_t *pPinHandler, uint8_t newState){
+
+	//We clear the position we need
+	pPinHandler ->pGPIOx ->ODR &= ~(SET << pPinHandler -> GPIO_PinConfig.GPIO_PinNumber);
+	if(newState == SET){
+		// The lower part of the register.
+		pPinHandler -> pGPIOx ->BSRR |= (SET << pPinHandler -> GPIO_PinConfig.GPIO_PinNumber);
+	}
+	else{
+		// The higher part of the register
+		pPinHandler -> pGPIOx ->BSRR |= (SET << (pPinHandler -> GPIO_PinConfig.GPIO_PinNumber + 16));
+	}
+
+}
+
+
+
+/* We create the function that reads the status of a specific pin.
+ * The IDR register is an input register that stores the logic
+ * value of the input pin, only by reading.
+ */
+
+
+
+uint32_t GPIO_ReadPin(GPIO_Handler_t *pPinHandler){
+	//create a new auxiliary variable
+	uint32_t pinValue = {0};
+
+	/*We load the value into the GPIOx_IDR,
+	 *
+	 * then shift it to the right. This register is used to store information
+	 * when the pin is an input.*/
+	pinValue = (pPinHandler -> pGPIOx -> IDR >> pPinHandler -> GPIO_PinConfig.GPIO_PinNumber);
+	/* We can see 0 or 1. The value changes depending on whether the HAL_GPIO_WritePin
+	function writes a 0 or 1 to the pin.
+	*/
+	pinValue &= 0b1;
+	return pinValue;
+
+}
+
+void GPIOxTooglePin(GPIO_Handler_t *pPinHandler){
+
+
+	//We create a new variable to store the state of the pin.
+	uint32_t pinState = {0};
+	// We create a new variable to store the operation XOR
+	uint32_t toogle		= {0};
+
+	/* To Determine the state of the pin, the value in the
+	 * IDR register is read, and a Bitwise AND operation is
+	 * performed to extract only the least significant bit
+	 */
+
+	pinState = (pPinHandler -> pGPIOx -> IDR >> pPinHandler -> GPIO_PinConfig.GPIO_PinNumber) & 0b1;
+
+	//We assign the value after the XOR operation
+	toogle = (pinState ^= SET) ;
+
+	/* If the pin state is 1, the variable 'toggle' is set to 0, the green LED is deactivated,
+	 * and the state is changed. Conversely, if the pin state is 0, the variable 'toggle' is set
+	 * to 1, the green LED is activated, and the state is changed again.
+	 */
+
+	if (toogle == SET){
+
+		// We change the position we need
+		pPinHandler ->pGPIOx ->ODR |=(SET << pPinHandler -> GPIO_PinConfig.GPIO_PinNumber);
+	}
+	else{
+		// We change the position we need
+		pPinHandler ->pGPIOx ->ODR &= ~(SET << pPinHandler -> GPIO_PinConfig.GPIO_PinNumber);
+	}
+
+}
+
+
