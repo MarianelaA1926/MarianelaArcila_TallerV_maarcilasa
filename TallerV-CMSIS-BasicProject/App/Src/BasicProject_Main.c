a5ab37646ed23f9eb2d1cb21e925d56b97f58c11
MarianelaA1926 <maarcilasa@unal.edu.co>
2023-05-17 22:26:00 -0500

    CMSIS-BasicConfig
    We created a base project that includes the external libraries

---
+++ TallerV-CMSIS-BasicProject/App/Src/BasicProject_Main.c
@@ -0,0 +1,24 @@
+/*
+ *
+ *
+ *  Created on: 24/03/2023
+ *      Author: marianela
+ */
+/**
+ ******************************************************************************
+ * @file           : BasicProyect_Main.c
+ * @author         : Marianela Arcila Sanchez- maarcilasa
+ * @brief          : The basic solution of a project with external libraries.
+ ******************************************************************************
+ * -
+ * -
+ * -
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+#include <stm32f4xx.h>
+#include "GPIOxDriver.h"
+#include "BasicTimer.h"
+#include "ExtiDriver.h"
+
